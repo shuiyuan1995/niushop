358cc6a2621d2027ef5388c2e057b73b7dd39ae5
zhupeiyuan <1353880213@qq.com>
2019-04-12 10:05:08 +0800

    删除代码

--- runtime/cache/22/ffeba601e5d9aa51492b33d70ea2ee.php
+++
@@ -1,3 +0,0 @@
-<?php
-//000000000000s:315:"E:\work\niushop01\runtime\cache\72\cafb13c91587426656cf3e6834bead.php,E:\work\niushop01\runtime\cache\29\6bb7fb6cbc12cc14dcdef0b055587f.php,D:\phpStudy\PHPTutorial\WWW\niushop\runtime\cache\38\d3ec86c5e26431a590ac32abb945b9.php,D:\phpStudy\PHPTutorial\WWW\niushop\runtime\cache\47\8fb08b2e5e8b66fd5aa73c0189af47.php";
-?>
--- version.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(!defined('NIU_VERSION')) {
-	define('NIU_VERSION', 'B2C免费2.3');
-	define('NIU_VER_DATE', '2018-11-02');
-	define('NIU_RELEASE', '20181102');
-	
-}
