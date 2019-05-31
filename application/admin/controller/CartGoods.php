e178c1c628961a968f5e52d75f05f68fb552e2d9
unwhisper <591856607@qq.com>
2019-05-31 18:07:43 +0800

    'cartlist'

--- application/admin/controller/CartGoods.php
+++ application/admin/controller/CartGoods.php
@@ -8,68 +8,26 @@
 
 namespace app\admin\controller;
 
-use data\service\SearchRank as SerarchServer;
+use data\service\CartList;
 
-class SearchRank extends BaseController
+class CartGoods extends BaseController
 {
     public function index()
     {
         if (request()->isAjax()) {
-            $search = new SerarchServer();
+            $cart = new CartList();
             $page_index = request()->post("page_index", 1);
             $page_size = request()->post('page_size', PAGESIZE);
-
-            $start_date = request()->post('start_date') == "" ? 0 : request()->post('start_date');
-            $end_date = request()->post('end_date') == "" ? 0 : request()->post('end_date');
-
-            $condition = array();
-            if ($start_date != 0 && $end_date != 0) {
-                $condition["search_time"] = [
-                    [
-                        ">",
-                        $start_date
-                    ],
-                    [
-                        "<",
-                        $end_date
-                    ]
-                ];
-            } elseif ($start_date != 0 && $end_date == 0) {
-                $condition["search_time"] = [
-                    [
-                        ">",
-                        $start_date
-                    ]
-                ];
-            } elseif ($start_date == 0 && $end_date != 0) {
-                $condition["search_time"] = [
-                    [
-                        "<",
-                        $end_date
-                    ]
-                ];
-            }
-            $list = $search->getKeyword($page_index, $page_size, $condition);
+            $list = $cart->getKeyword($page_index, $page_size);
 
             return $list;
         }else{
-            return view($this->style . "SearchRank/index");
+            return view($this->style . "CartGoods/index");
         }
-    }
-
-    public function delete()
-    {
-        $id = request()->post('id');
-        $search = new SerarchServer();
-        $result = $search->deleteKeyword($id);
-        if ($result){
-            return returnAjax(1,'删除成功');
-        }
-        return returnAjax(-1,'删除失败');
     }
 
     public function excelList(){
-        $search = new SerarchServer();
+        $cart = new CartList();
         $start_date = request()->post('start_date') == "" ? 0 : request()->post('start_date');
         $end_date = request()->post('end_date') == "" ? 0 : request()->post('end_date');
 
@@ -100,21 +58,21 @@ class SearchRank extends BaseController
                 ]
             ];
         }
-        $list = $search->getKeyword(1, 0, $condition);
+        $list = $cart->getKeyword(1, 0, $condition);
 
         return $list;
     }
 
     public function dataExcel()
     {
-        $xlsName  = "搜索排行列表";
+        $xlsName  = "扫码访问列表";
         $xlsCell  = array(
             array('keyword','搜索关键词'),
             array('search_times','搜索次数'),
             array('search_time','最后搜索时间')
         );
 
-        $search = new SerarchServer();
+        $cart = new CartList();
         $start_date = request()->get('start_date') == "" ? 0 : request()->post('start_date');
         $end_date = request()->get('end_date') == "" ? 0 : request()->post('end_date');
 
@@ -145,7 +103,7 @@ class SearchRank extends BaseController
                 ]
             ];
         }
-        $list = $search->getKeyword(1, 0, $condition);
+        $list = $cart->getKeyword(1, 0, $condition);
         $list = $list["data"];
         dataExcel($xlsName,$xlsCell,$list);
     }
---
+++ data/service/CartList.php
@@ -0,0 +1,53 @@
+<?php
+namespace data\service;
+
+use think\Db;
+use data\api\ICartList;
+
+class CartList implements ICartList
+{
+    public function getKeyword($page_index = 1, $page_size = 0, $condition = '', $field = '*')
+    {
+        $count = Db::query('SELECT COUNT(DISTINCT  `buyer_id`) as count FROM `ns_cart`');
+        $count = $count[0]['count'];
+
+        $user = Db::query('SELECT DISTINCT(c.buyer_id),m.member_name FROM `ns_cart` c INNER JOIN `ns_member` m ON c.buyer_id=m.uid');
+
+        if ($page_index == 1 && $page_size == 0){
+            foreach ($user as $k => $v){
+                $data[$v['member_name']] = Db::name('ns_cart')
+                    ->alias('c')
+                    ->join('ns_member m','c.buyer_id = m.uid')
+                    ->where($condition)
+                    ->where('buyer_id',$v['buyer_id'])
+                    ->order('c.cart_id','desc')
+                    ->select();
+            }
+            $page_count = 1;
+        }else{
+            foreach ($user as $k => $v){
+                $data[$v['member_name']] = Db::name('ns_cart')
+                                            ->alias('c')
+                                            ->join('ns_member m','c.buyer_id = m.uid')
+                                            ->where($condition)
+                                            ->where('buyer_id',$v['buyer_id'])
+                                            ->order('c.cart_id','desc')
+                                            ->page($page_index,$page_size)
+                                            ->select();
+            }
+            $page_count = ceil($count/$page_size);
+        }
+
+        if ($data){
+            $list['total_count'] = $count;
+            $list['page_count'] = $page_count;
+            $list['data'] = $data;
+            return $list;
+        }else{
+            $list['total_count'] = 0;
+            $list['page_count'] = 0;
+            $list['data'] = '';
+            return $list;
+        }
+    }
+}
