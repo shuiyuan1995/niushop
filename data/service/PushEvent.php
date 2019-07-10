970083a5d54c963b6f1500196a5cc3ea7c8122bb
unwhisper <591856607@qq.com>
2019-07-10 14:41:30 +0800

    'add'

---
+++ data/service/PushEvent.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: whisper
+ * Date: 2019/7/9
+ * Time: 11:38
+ */
+
+namespace data\service;
+
+
+class PushEvent
+{
+    /**
+     * @var string 目标用户id
+     */
+    protected $to_user = '';
+
+    /**
+     * @var string 推送服务地址
+     */
+    protected $push_api_url = 'http://132.148.158.244:3121/';
+
+    /**
+     * @var string 推送内容
+     */
+    protected $content = '';
+
+    /**
+     * 设置推送用户，若参数留空则推送到所有在线用户
+     *
+     * @param string $user
+     * @return $this
+     */
+    public function setUser($user = '')
+    {
+        $this->to_user = $user ? : '';
+        return $this;
+    }
+
+    /**
+     * 设置推送内容
+     *
+     * @param string $content
+     * @return $this
+     */
+    public function setContent($content = '')
+    {
+        $this->content = $content;
+        return $this;
+    }
+
+    /**
+     * 推送
+     */
+    public function push()
+    {
+        $data = [
+            'type' => 'publish',
+            'content' => $this->content,
+            'to' => $this->to_user,
+        ];
+        $ch = curl_init ();
+        curl_setopt($ch, CURLOPT_URL, $this->push_api_url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:'));
+        $res = curl_exec($ch);
+        curl_close($ch);
+    }
+}
